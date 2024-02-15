895c7da1e5b80deaed52eed14f43071b89216b00
abeer.sohail <abeer.sohail@invixible.com>
2024-02-15 13:55:43 +0500

    Made some changes

--- compare_db_objects.py
+++ compare_db_objects.py
@@ -1,51 +1,59 @@
 from datetime import datetime, timedelta
 import json
 
-def get_time(obj):
-        time_int = int(float(obj['time']['$date']['$numberLong'])/1000)
-        return datetime.fromtimestamp(time_int)
+# Not working as expected
+# Need a more precise time and data matching algorithm
 
 def get_value_within_bounds(msg, min_val, max_val):
         val = None
         while True:
                 val = int(input(msg + " "))
                 if val < min_val or val > max_val:
-                        print('ERROR! Value must be between', min_val, 'and', 'max_val')
+                        print('ERROR! Value must be between', min_val, 'and', max_val)
                 else:
                         break
         
         return val
 
 def compare_properties(obj1, obj2, prop):
-        value_match = obj1[prop]['value'] == obj2[prop]['value']
-        unit_match = obj1[prop]['unit'] == obj2[prop]['unit']
-        return value_match and unit_match
+        try:
+                value_match = obj1['reading'][prop]['value'] == obj2['reading'][prop]['value']
+                unit_match = obj1['reading'][prop]['unit'] == obj2['reading'][prop]['unit']
+                return_val = (value_match and unit_match)
+                return return_val
+        except KeyError:
+                print('Key Error: Missed param', prop, 'between obj1 id', obj1['_id']['$oid'], 'and obj2 id', obj2['_id']['$oid'])
+                return False
 
-def sort_object_list_using_nested_property(lst, property_chain):
-    """
-    Sorts a list of dictionaries based on a nested property.
+# def sort_object_list_using_nested_property(lst, property_chain):
+#     """
+#     Sorts a list of dictionaries based on a nested property.
 
-    Args:
-    lst: The list of dictionaries to sort.
-    property_chain: A list of keys representing the nested property path.
+#     Args:
+#     lst: The list of dictionaries to sort.
+#     property_chain: A list of keys representing the nested property path.
 
-    Returns:
-    The sorted list of dictionaries.
-    """
-    def get_nested_property(obj, property_chain):
-        if not property_chain:
-            return obj
-        return get_nested_property(obj.get(property_chain[0], {}), property_chain[1:])
+#     Returns:
+#     The sorted list of dictionaries.
+#     """
+#     def get_nested_property(obj, property_chain):
+#         if not property_chain:
+#             return obj
+#         return get_nested_property(obj.get(property_chain[0], {}), property_chain[1:])
 
-    return sorted(lst, key=lambda x: get_nested_property(x, property_chain))
+#     return sorted(lst, key=lambda x: get_nested_property(x, property_chain))
 
-filenames = ['monitorings-12290-ec2.json', 'monitorings-12290-k8s.json']
+filenames = ['monitorings-ec2.json', 'monitorings-k8s.json']
 data = []
+sensorId = ''
 
 for filename in filenames:
         with open(filename) as f:
                 # Load JSON data from file
-                loaded_data = sort_object_list_using_nested_property(json.load(f), ['time', '$date', '$numberLong'])
+                loaded_data = json.load(f)
+                sensorId = loaded_data[0]['sensorId']
+                for i in range(len(loaded_data)):
+                        loaded_data[i]['time'] = int(loaded_data[i]['time']['$date']['$numberLong'])
                 data.append(loaded_data)
                 # Now data is a Python dictionary
 
@@ -53,12 +61,12 @@ number_of_docs = len(data[0])
 
 good_time_count = 0
 bad_time_count = 0
-time_threshold_sec = get_value_within_bounds('Enter time threshold (sec):', 5, 500)
+time_threshold_millisec = get_value_within_bounds('Enter time threshold (sec):', 5, 500) * 1000
 
 good_value_match_count = 0
 bad_value_match_count = 0
 
-reading_sensors = ['Speed', 'Satellites', 'Movement', 'BatteryCurrent', 'Battery', 'Ignition', 'GNSS_Status', 'TowDetection', 'ServerTime', 'GSMSignal', 'BatteryVoltage', 'ExternalVoltage']
+reading_sensors = ['Speed', 'Satellites', 'Movement', 'BatteryCurrent', 'Battery', 'Ignition', 'GNSS_Status', 'TowDetection', 'GSMSignal', 'BatteryVoltage', 'ExternalVoltage']
 
 # max_readings_keys = []
 # max_readings = 0
@@ -66,7 +74,7 @@ reading_sensors = ['Speed', 'Satellites', 'Movement', 'BatteryCurrent', 'Battery
 for i in range(number_of_docs):
         obj1 = data[0][i]
         obj2 = data[1][i]
-        time_diff = get_time(obj1) - get_time(obj2)
+        time_diff = obj1['time'] - obj2['time']
 
         # readings = data[0][i]['reading'].keys()
         # number_of_readings= len(readings)
@@ -75,8 +83,8 @@ for i in range(number_of_docs):
         #         max_readings = number_of_readings
         #         max_readings_keys = readings
         
-        time_cond1 = time_diff < timedelta(seconds=time_threshold_sec)
-        time_cond2 = time_diff > timedelta(seconds=-time_threshold_sec)
+        time_cond1 = time_diff < time_threshold_millisec
+        time_cond2 = time_diff > -time_threshold_millisec
 
         if time_cond1 and time_cond2:
                 good_time_count += 1
@@ -84,12 +92,14 @@ for i in range(number_of_docs):
                 bad_time_count += 1
         
         for sensor in reading_sensors:
-                if ~compare_properties(obj1['reading'], obj2['reading'], sensor):
+                if not compare_properties(obj1, obj2, sensor):
                         bad_value_match_count +=1
                         break
 
 good_value_match_count = number_of_docs - bad_value_match_count
 
+print('\nTime threshold', time_threshold_millisec / 1000, 'sec')
+print('SensorId', sensorId)
 print('Docs within time threshold', good_time_count)
 print('Docs outside time threshold', bad_time_count)
 print('Good value match count', good_value_match_count)
