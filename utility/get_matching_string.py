eb509bad6118fb5f2654921ec356a5361816c176
abeer.sohail <abeer.sohail@invixible.com>
2024-04-22 20:11:42 +0500

    some work

--- utility/get_matching_string.py
+++ utility/get_matching_string.py
@@ -1,18 +1,21 @@
 import re
 from ignore_constants import *
 
-def find_matches(pattern, text, sort=False):
+def find_matches(pattern, text, sort=False, unique=False):
     # The pattern matches "Bike" followed by exactly 5 digits
     matches = re.findall(pattern, text)
     # Convert the matched strings to integers
 
+    if unique:
+        matches = list(set(matches))
     if sort:
         matches = sorted(matches)
+
 
     return matches
 
 # Example usage
 pattern = r'Bike (\d{5})' # r"Bike\s\d{5}\s\((?P<digits>\d{15})"
 
-numbers = find_matches(pattern, text, True)
+numbers = find_matches(pattern, text, True, True)
 print('\n'.join(numbers))
--- utility/manipulate_csv.py
+++ utility/manipulate_csv.py
@@ -6,9 +6,15 @@ def delete_columns(col):
     global df
     df.drop(columns=col, inplace=True)
 
-def rearrange_columns(order):
+def rearrange_columns(order, delete_others=False):
     global df
+    print(Warning("All column names must be unqiue! Expect weird behavior otherwise."))
     df = df[order]
+
+    if delete_others:
+        all_cols = list(df.columns)
+        cols_to_delete = list(set(all_cols) - set(order))
+        delete_columns(cols_to_delete)
 
 def generate_dict(input_file_path, int_key = False):
     with open(input_file_path, 'r') as file:
@@ -33,9 +39,9 @@ def convert_epoch_to_iso(column_name, unit='s'):
     # Convert datetime to ISO format
     df['time'] = df['time'].apply(lambda x: x.isoformat() if notnull(x) else None)
 
-def replace_substring_from_columns(substring, to_replace):
+def replace_substring_from_columns(substring, to_replace_with):
     global df
-    df.columns = df.columns.str.replace(substring, to_replace)
+    df.columns = df.columns.str.replace(substring, to_replace_with)
 
 def move_column_to_position(column, pos):
     global df
@@ -45,28 +51,55 @@ def move_column_to_position(column, pos):
     # Step 2: Insert the column at the desired position
     df.insert(pos, column_to_move.name, column_to_move)
 
+def remap_values(v1, v2, mapping_file_path, insert_index):
+    global df
+    v1_v2_name_mapping = generate_dict(mapping_file_path)
+    df.insert(insert_index, v1, df[v2].map(v1_v2_name_mapping))
+
+def filter_columns(col_names, condition=None, print_output=False):
+    global df
+    result = df.loc[condition, col_names]
+    
+    if print_output:
+        print(result)
+    
+    return result
+        
+
+base_path = '/home/sarwan/Downloads'
+
 number_of_files = 1
-sensor_id_name_mapping = generate_dict('/home/sarwan/Downloads/salesforce-devices-name.csv')
-print(sensor_id_name_mapping)
 
 for i in range(1, number_of_files + 1):
-    input_path = '/home/sarwan/Downloads/salesforce-monitorings-dump.json'
-    output_path = '/home/sarwan/Downloads/salesforce-monitorings-data-final.csv'
+    input_path = f'{base_path}/all-rensair-license-devices.json'
+    output_path = f'{base_path}/all-air-purifier-devices.csv'
 
     with open(input_path, 'r') as file:
         data = load(file)
 
     df = json_normalize(data)
 
-    df.insert(1, 'name', df['sensorId'].map(sensor_id_name_mapping))
+    rearrange_columns(["name", "internalId", "deviceType", "tenant", "packetFromPlatform.c8y_Hardware.serialNumber", "packetFromPlatform.c8y_Mobile.cellId", "packetFromPlatform.c8y_Mobile.iccid", "packetFromPlatform.c8y_Mobile.imei", "packetFromPlatform.c8y_Mobile.imsi", "packetFromPlatform.c8y_Mobile.lac", "packetFromPlatform.c8y_Mobile.mcc", "packetFromPlatform.c8y_Mobile.mnc"])
 
-    convert_epoch_to_iso('time.$date.$numberLong', 'ms')
+    replace_substring_from_columns("packetFromPlatform.", "")
+    replace_substring_from_columns("c8y_Mobile.", "")
+    replace_substring_from_columns("c8y_Hardware.", "")
 
-    delete_columns(['tenant', 'time.$date.$numberLong'])
+    # condition = df['iccid'].notnull() & df['iccid'].ne('')
 
-    replace_substring_from_columns('reading.', '')
+    # print(condition)
 
-    move_column_to_position('time', 3)
+    # filter_columns(["name", "iccid"], condition, True).to_csv(f'{base_path}/result.csv', index=False)
+
+    # remap_values(v1, v2, mapping_file_path, 1)
+
+    # convert_epoch_to_iso('time.$date.$numberLong', 'ms')
+
+    # delete_columns(['tenant', 'time.$date.$numberLong'])
+
+    # replace_substring_from_columns('reading.', '')
+
+    # move_column_to_position('time', 3)
 
     print(df.head(20))
 
