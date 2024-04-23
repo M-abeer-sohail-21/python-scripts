1799392ed2940157fa500fd3d77ff9369276036f
abeer.sohail <abeer.sohail@invixible.com>
2024-04-23 15:40:18 +0500

    Some work

---
+++ utility/get_bike_name_internalId.py
@@ -0,0 +1,31 @@
+from json import load
+from pandas import json_normalize
+
+def find_matches(my_string, pattern):
+
+    match = find(pattern, my_string)
+    if match:
+        return match
+    else:
+        return None
+
+
+data_file_path = './utility/down_bikes.json'
+
+with open(data_file_path, 'r') as f:
+    data = load(f)
+
+df = json_normalize(data)
+
+df['bike_number'] = df['name'].str.extract(r'\b(\d{5})\b', expand=False)
+
+# print(df[['bike_number','internalId']])
+# print(list(df['bike_number']))
+# print(list(df['internalId']))
+print(
+    list(
+        map(
+            lambda x:list(df[x]), ['bike_number', 'internalId']
+        )
+    )
+)
